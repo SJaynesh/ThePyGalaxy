4a56671d7aa48ffe59d1f49f1fe4c331ddff2241
SJaynesh <jayneshsodvadiya@gmail.com>
2024-11-18 19:59:02 +0530

    PY

---
+++ Ch-1 Introduction and Fundament of  Python/hello.py
@@ -0,0 +1,24 @@
+
+# To Print any Message
+
+print("Hello World")
+
+# To Input User
+
+name = input("Enter your name: ")
+
+print("Your Name : ", name)
+
+# Int Input
+
+a = int(input("Enter any number : "))
+print("A : ",a )
+
+# Float Input 
+
+PI = float(input("Enter PI value : "))
+print("PI : ",PI)
+
+# In Detail Print
+
+print("I am ","Jaynesh","--","Sarkar")
